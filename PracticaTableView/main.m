6e2288fd0d357c3c3146212c18813efc20ae9189
ITESM CCM <Laboratorios@ITESMs-iMac-159.local>
2014-10-11 08:52:37 -0500

    Initial Commit

---
+++ PracticaTableView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PracticaTableView
+//
+//  Created by ITESM CCM on 10/11/14.
+//  Copyright (c) 2014 ITESM CCM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
